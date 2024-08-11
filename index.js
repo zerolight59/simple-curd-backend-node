1119ff1508318f8c911c21d5bef2d9b67925307c
MANU SANKAR U <115739592+zerolight59@users.noreply.github.com>
2024-08-12 01:44:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const app = express()
+const ProductRoute =require("./routes/product.route.js")
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+
+//routes
+app.use("/api/products",ProductRoute)
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hello from node API servers updated");
+});
+
+
+
+mongoose.connect("mongodb+srv://manusankaru10:YG00p7ihilxZLqXx@backenddb.5i76j.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+.then(()=>{
+    console.log("connected to database")
+    app.listen(3000,()=>{
+        console.log("server is running at the port 3000")
+    })
+})
+.catch(()=>{
+    console.log("connected failed")
+})
